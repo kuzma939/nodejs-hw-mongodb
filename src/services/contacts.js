bdbe85dac9e0c299077ff190723aaa9e73741845
kuzma939 <135020134+kuzma939@users.noreply.github.com>
2024-08-07 10:17:50 +0200

    Update contacts.js

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,31 +1,13 @@
 import { ContactsCollection } from '../db/models/contacts.js';
-import mongoose from 'mongoose'; 
 
-export const getContactsById = async (contactId) => {
-  if (!mongoose.Types.ObjectId.isValid(contactId)) {
-    throw new Error('Invalid contact ID format');
-  }
-  try {
-    // Використовуємо findById для Mongoose
-    const contact = await ContactsCollection.findById(contactId);
-    if (!contact) {
-      throw new Error('Contact not found');
-    }
-    return contact;
-  } catch (error) {
-    console.error(`Error fetching contact with id ${contactId}: ${error.message}`);
-    throw new Error('Failed to fetch contact');
-  }
+const getAllContacts = async () => {
+  const contacts = await ContactsCollection.find();
+  return contacts;
 };
 
-export const getAllContacts = async () => {
-  try {
-    // Використовуємо find() для Mongoose
-    const contacts = await ContactsCollection.find();
-    
-    return contacts;
-  } catch (error) {
-    console.error('Error fetching contacts:', error.message);
-    throw new Error('Failed to fetch contacts'); 
-  }
+const getContactsById = async (contactId) => {
+  const contact = await ContactsCollection.findById(contactId);
+  return contact;
 };
+
+export { getAllContacts, getContactsById };
