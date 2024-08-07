b52a119a953eaba050b71ec989e44278b4b07b8a
kuzma939 <135020134+kuzma939@users.noreply.github.com>
2024-08-07 09:48:34 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import { initMongoConnection } from './db/initMongoConnection.js';
-import { ContactsCollection } from './db/models/contacts.js';
 import { setupServer } from './server.js';
 
 const bootstrap = async () => {
   await initMongoConnection();
-  const contacts = await ContactsCollection.find({}).exec();
-  console.log(contacts); // Для перевірки, що дані отримуються правильно
+
   setupServer();
 };
 
