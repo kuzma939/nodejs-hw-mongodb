4605bb2e40f88f13a453dba4e0d70273a5f6f016
kuzma939 <135020134+kuzma939@users.noreply.github.com>
2024-08-25 20:58:47 +0200

    Update auth.js

--- src/services/auth.js
+++ src/services/auth.js
@@ -17,28 +17,36 @@ export const registerUser = async (payload) => {
    
 };
 export const loginUser = async (payload) => {
-    const user = await UsersCollection.findOne({ email: payload.email });
-    if (!user) {
-      throw createHttpError(404, 'User not found');
-    }
-    const isEqual = await bcrypt.compare(payload.password, user.password); // Порівнюємо хеші паролів
-  
-    if (!isEqual) {
-      throw createHttpError(401, 'Unauthorized');
-    }
-    await SessionsCollection.deleteOne({ userId: user._id });
+  const user = await UsersCollection.findOne({ email: payload.email });
+  if (!user) {
+    throw createHttpError(404, 'User not found');
+  }
+
+  const isEqual = await bcrypt.compare(payload.password, user.password);
+
+  if (!isEqual) {
+    throw createHttpError(401, 'Unauthorized');
+  }
+  if (!user._id) {
+    throw createHttpError(500, 'User ID not found');
+  }
+  await SessionsCollection.deleteOne({ userId: user._id });
 
   const accessToken = randomBytes(30).toString('base64');
   const refreshToken = randomBytes(30).toString('base64');
-
-  return await SessionsCollection.create({
+  await SessionsCollection.create({
     userId: user._id,
     accessToken,
     refreshToken,
     accessTokenValidUntil: new Date(Date.now() + FIFTEEN_MINUTES),
     refreshTokenValidUntil: new Date(Date.now() + ONE_DAY),
   });
- 
+
+  // Повертаємо відповідь без поля userId
+  return {
+    accessToken,
+    refreshToken,
+  };
 };
 export const logoutUser = async (sessionId) => {
   await SessionsCollection.deleteOne({ _id: sessionId });
@@ -71,8 +79,6 @@ export const refreshUsersSession = async ({ sessionId, refreshToken }) => {
     await SessionsCollection.deleteOne({ _id: sessionId });
     throw createHttpError(401, 'Session token expired');
   }
-
-  // Перевірка наявності session.userId
   if (!session.userId) {
     throw createHttpError(500, 'Session user ID not found');
   }
