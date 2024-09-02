6cc33aef63fe1c54530d1e65b585bd9c29276d33
kuzma939 <135020134+kuzma939@users.noreply.github.com>
2024-09-02 13:41:52 +0200

    Update auth.js

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -80,6 +80,7 @@ export const requestResetEmailController = async (req, res) => {
   });
 };
 export const resetPasswordController = async (req, res) => {
+  
   await resetPassword(req.body);
   res.json({
     message: 'Password was successfully reset!',
