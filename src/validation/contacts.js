168e1dc682858e2bdaad05691d9c718ae6d209b9
kuzma939 <135020134+kuzma939@users.noreply.github.com>
2024-09-04 16:23:01 +0200

    Update contacts.js

--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -15,12 +15,8 @@ export const createContactsSchema = Joi.object({
   
   export const updateContactsSchema = Joi.object({
     name: Joi.string().min(3).max(20),
-    phoneNumber: Joi.string()
-      .pattern(/^\+?[0-9]{10,15}$/)
-      .required(),
+    phoneNumber: Joi.string().pattern(/^\+?[0-9]{10,15}$/),
     email: Joi.string().email(),
     isFavourite: Joi.boolean(),
-    contactType: Joi.string()
-  
-      .valid('work', 'home', 'personal'),
+    contactType: Joi.string().valid('work', 'home', 'personal'),
   });
